7013e3bd8d8b91b8d92f7c5207cce67df191b7f3
webdiego <webdiegomassarini@gmail.com>
2023-01-03 22:37:01 +0100

    feat(loading)

--- components/DatePicker.tsx
+++ components/DatePicker.tsx
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { DayPicker } from 'react-day-picker';
 import Time from './Time';
-
+import Image from 'next/image';
 export default function DatePicker() {
   const [selected, setSelected] = React.useState<Date>();
+  const [loading, setLoading] = React.useState<boolean>(false);
   const disabledDays = [{ from: new Date(), to: new Date(new Date().getFullYear(), 12, 31) }];
+
+  useEffect(() => {
+    if (selected) {
+      setTimeout(() => {
+        setLoading(false);
+      }, 1000);
+    }
+  }, [selected]);
 
   return (
     <>
@@ -14,11 +23,16 @@ export default function DatePicker() {
         toYear={new Date().getFullYear()}
         captionLayout="dropdown"
         selected={selected}
-        onSelect={setSelected}
+        onSelect={(selected) => {
+          setSelected(selected);
+          setLoading(true);
+        }}
         disabled={disabledDays}
       />
-
-      {selected && <Time {...{ selected }} />}
+      <div className="h-32 flex items-center">
+        {loading && <Image src={'/clock-anim.svg'} width={20} height={20} alt="logo" />}
+        {selected && !loading && <Time {...{ selected }} />}
+      </div>
     </>
   );
 }
